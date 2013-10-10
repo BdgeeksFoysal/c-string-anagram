86f415626cc75a6ec2564512b710a490ce405b27
foysal <cfaion341@gmail.com>
2013-10-10 15:27:18 +0200

    working version from professor's code

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
 
 int main(void){
-	
+	char s1[MAX] = "hithere", s2[MAX] = "hellothere";
+	int occurences[26], i, flag;
+
+	//populating occurences with 0 value for each array element
+	for (i = 0; i < 26; ++i){
+		occurences[i] = 0;
+	}
+
+	//mother of inteligence!
+	for (i = 0; i < strlen(s1); ++i){
+		occurences[tolower(s1[i]) - 'a']++;
+	}
+
+	for (i = 0; i < strlen(s2); ++i){
+		occurences[tolower(s2[i]) - 'a']--;
+	}
+
+	flag = 1;
+
+	for (i = 0; i < 26 && flag; ++i){
+		if (occurences[i] != 0){
+			flag = 0;
+		}
+	}
+
+
 	return 0;
 }
